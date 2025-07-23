5ab8f591bedde13c61ba61aa3ad98c6d49f73574
soyacen <soyacen@outlook.com>
2025-07-23 10:00:30 +0800

    feat: add basicauth

--- middleware/basicauth/middleware.go
+++ middleware/basicauth/middleware.go
@@ -1,8 +1,29 @@
 package basicauth
 
 import (
+	"context"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
+
+type ctxKey struct{}
+
+func FromContext(ctx context.Context) (string, bool) {
+	value, ok := ctx.Value(ctxKey{}).(string)
+	return value, ok
+}
+
+type Account struct {
+	User     string
+	Password string
+}
+
+type Accounts []Account
 
 type options struct {
 	realm string
@@ -18,7 +39,9 @@ func (o *options) apply(opts ...Option) *options {
 type Option func(o *options)
 
 func defaultOptions() *options {
-	return &options{}
+	return &options{
+		realm: "Authorization Required",
+	}
 }
 
 func Realm(realm string) Option {
@@ -27,7 +50,54 @@ func Realm(realm string) Option {
 	}
 }
 
-func Middleware(accounts gin.Accounts, opts ...Option) gin.HandlerFunc {
+func Middleware(accounts Accounts, opts ...Option) gin.HandlerFunc {
 	opt := defaultOptions().apply(opts...)
-	return gin.BasicAuthForRealm(accounts, opt.realm)
+	realm := "Basic realm=" + strconv.Quote(opt.realm)
+	pairs := processAccounts(accounts)
+	return func(c *gin.Context) {
+		user, found := pairs.searchCredential(c.GetHeader("Authorization"))
+		if !found {
+			c.Header("WWW-Authenticate", realm)
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		c.Request = c.Request.WithContext(context.WithValue(c.Request.Context(), ctxKey{}, user))
+		c.Next()
+	}
+}
+
+func processAccounts(accounts Accounts) authPairs {
+	length := len(accounts)
+	if length <= 0 {
+		panic(errors.New("basicauth: empty list of authorized credentials"))
+	}
+	pairs := make(authPairs, 0, length)
+	for _, account := range accounts {
+		if account.User == "" {
+			panic(errors.New("basicauth: user can not be empty"))
+		}
+		base := account.User + ":" + account.Password
+		value := "Basic " + base64.StdEncoding.EncodeToString([]byte(base))
+		pairs = append(pairs, authPair{value: value, user: account.User})
+	}
+	return pairs
+}
+
+type authPair struct {
+	value string
+	user  string
+}
+
+type authPairs []authPair
+
+func (a authPairs) searchCredential(authValue string) (string, bool) {
+	if authValue == "" {
+		return "", false
+	}
+	for _, pair := range a {
+		if subtle.ConstantTimeCompare([]byte(pair.value), []byte(authValue)) == 1 {
+			return pair.user, true
+		}
+	}
+	return "", false
 }
